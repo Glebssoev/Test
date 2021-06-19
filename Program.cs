0d7961d6c797b43230c7e530a65faa6044e58247
Glebssoev <glebssev@gmail.com>
2021-06-19 22:04:30 +0300

    'ahahahha'

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 8.9;
+            double c = 9.2;
+            int sum = Convert.ToInt32(a + c);
+            Console.WriteLine(sum);
+            if (sum >= 18.1)
+            {
+                Console.WriteLine(typeof(MessageProcessingHandler));
+            }
+        }
+    }
+}
